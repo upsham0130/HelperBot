e9d2aea49c824f4550bb45d399e9f170f5ec267f
Upsham Naik <upshamgnaik@outlook.com>
2019-07-19 17:04:21 -0400

    sheets api added and testing for images

--- app.py
+++ app.py
@@ -1,21 +1,31 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from pprint import pprint
+
+scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+client = gspread.authorize(creds)
+sheet = client.open("WhatsappBot Data").sheet1
+data = sheet.get_all_records()
+row = sheet.col_values(1)
+pprint(row)
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def default():
+    return ""
 
-@app.route("/sms", methods=['POST'])
+@app.route("/sms", methods=['GET', 'POST'])
 def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Fetch the message
-    msg = request.form.get('Body')
+    # Fetch message
+    msg = request.form.get('To')
 
     # Create reply
     resp = MessagingResponse()
-    resp.message("You said: {}".format(msg))
+    resp.message("Your response: {}".format(msg))
 
     return str(resp)
 
